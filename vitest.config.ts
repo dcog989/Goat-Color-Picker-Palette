668a8e0deaf62092fb00b42a4ccc647a86a1f13e
dcog989 <sirdavidlaws@gmail.com>
2026-02-03 22:35:10 +0000

    path format

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vitest/config';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
     plugins: [svelte({ hot: !process.env.VITEST })],
