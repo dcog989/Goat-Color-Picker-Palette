ed9f34527fefd137d10abec1205d5796788ff636
dcog989 <sirdavidlaws@gmail.com>
2026-09-23 21:39:53 +0100

    fix(hsl-sliders): preserve hue/saturation when lightness hits 0 or 100

--- src/lib/components/HslSliders.svelte
+++ src/lib/components/HslSliders.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+import { untrack } from "svelte";
 import { getApp } from "../context";
 import { getGradient } from "../utils/gradients";
 import Slider from "./Slider.svelte";
@@ -10,7 +11,14 @@ let localHsl = $state({ h: 0, s: 0, l: 0 });
 $effect(() => {
   if (color.mode === "hsl") {
     const hsl = color.hslComp;
-    localHsl = { h: hsl.h, s: hsl.s, l: hsl.l };
+    if (hsl.l <= 0 || hsl.l >= 100) {
+      // Black/white carry no hue or saturation, so retain the current slider
+      // values; otherwise raising lightness would reset the color to grey.
+      const prev = untrack(() => localHsl);
+      localHsl = { h: prev.h, s: prev.s, l: hsl.l };
+    } else {
+      localHsl = { h: hsl.h, s: hsl.s, l: hsl.l };
+    }
   }
 });
 
@@ -24,7 +32,7 @@ const updateHslFromLocal = () => color.setHslValues(localHsl.h, localHsl.s, loca
   min={0}
   max={360}
   step={1}
-  {...getGradient('hsl-h', color.rgbComp)}
+  {...getGradient("hsl-h", color.rgbComp)}
   oninput={updateHslFromLocal}
 />
 <Slider
@@ -34,7 +42,7 @@ const updateHslFromLocal = () => color.setHslValues(localHsl.h, localHsl.s, loca
   min={0}
   max={100}
   step={1}
-  {...getGradient('hsl-s', color.rgbComp)}
+  {...getGradient("hsl-s", color.rgbComp)}
   oninput={updateHslFromLocal}
 />
 <Slider
@@ -44,6 +52,6 @@ const updateHslFromLocal = () => color.setHslValues(localHsl.h, localHsl.s, loca
   min={0}
   max={100}
   step={1}
-  {...getGradient('hsl-l', color.rgbComp)}
+  {...getGradient("hsl-l", color.rgbComp)}
   oninput={updateHslFromLocal}
 />
